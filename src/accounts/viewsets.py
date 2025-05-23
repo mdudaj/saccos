94a8f93f3d80aab072a04fff2b9a856a5d2c51eb
John Mduda <john.mduda@nimr.or.tz>
2025-05-24 02:32:23 +0300

    fet: User management and roles

---
+++ src/accounts/viewsets.py
@@ -0,0 +1,83 @@
+# -*- encoding: utf-8 -*-
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group, Permission
+from django.utils.translation import gettext_lazy as _
+from viewflow import Icon
+from viewflow.forms import FieldSet, Layout, Row
+from viewflow.urls import Application, DeleteViewMixin, DetailViewMixin, ModelViewset
+
+User = get_user_model()
+
+
+class UserViewset(DeleteViewMixin, DetailViewMixin, ModelViewset):
+    """
+    Manager system users (Administrators, Loan Officers, Secretariate committee, etc.)
+    """
+    icon = Icon('person')
+    model = User
+    list_columns = ('email', 'first_name', 'last_name', 'is_active', 'is_staff')
+    list_searh_fields = ('email', 'first_name', 'last_name')
+    list_filter_fields = ('is_active', 'is_staff')
+
+    queryset = User.objects.select_related().all().order_by('-created')
+
+    form_layout = Layout(
+        "email",
+        FieldSet(
+            _("Personal Information"),
+            Row('first_name', 'last_name'),
+        ),
+        FieldSet(
+            _("Account Settings"),
+            Row('is_active', 'is_staff'),
+        ),
+        FieldSet(
+            _("Permissions"),
+            Row('groups'),
+        ),
+    )
+
+
+class GroupViewset(DeleteViewMixin, DetailViewMixin, ModelViewset):
+    """
+    Manages roles and permissions for users.
+    """
+    icon = Icon('security')
+    model = Group
+    list_columns = ('name','permissions_count')
+    list_searh_fields = ('name',)
+    list_filter_fields = ('permissions',)
+    
+    queryset = Group.objects.prefetch_related('permissions').order_by('name')
+
+    def permissions_count(self, obj):
+        """
+        Returns the number of permissions associated with the group.
+        """
+        return obj.permissions.count()
+    permissions_count.short_description = _("Permissions Count")
+
+    form_layout = Layout(
+        "name",
+        FieldSet(
+            _("Permissions"),
+            Row('permissions'),
+        ),
+    )
+    
+
+class UserAdminApp(Application):
+    """
+    User management application.
+    """
+    title = _("User Management")
+    app_name = "accounts"
+    icon = Icon('people')
+
+    viewsets = [
+        UserViewset(),
+        GroupViewset(),
+    ]
+
+        
--- src/core/urls.py
+++ src/core/urls.py
@@ -1,17 +1,22 @@
 # -- coding: utf-8 -*-
-from django.urls import path
 from django.contrib.auth import get_user_model
+from django.urls import path
 from viewflow.contrib.auth import AuthViewset
-from viewflow.urls import Application, Site, ModelViewset
+from viewflow.urls import Application, ModelViewset, Site
 
+from accounts.viewsets import UserAdminApp
 from members import models
 
-site = Site(title="NIMR SACCOS", viewsets=[
-    Application(
-        title="Members", icon="people", app_name="members", viewsets=[
-            ModelViewset(model=models.MemberProfile),
-        ]
-    ),
+site = Site(title="NIMR SACCOS LTD.", viewsets=[
+    UserAdminApp(),
+    # Application(
+    #     title="Member Management", 
+    #     icon="people", 
+    #     app_name="members", 
+    #     viewsets=[
+    #         ModelViewset(model=models.MemberProfile),
+    #     ]
+    # ),
 ])
 
 urlpatterns = [
