7094d8da244829738f6d1c8486436cf8338598d2
John Mduda <john.mduda@nimr.or.tz>
2025-05-23 03:29:53 +0300

    Initial commit

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,22 +1,20 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
+# -- coding: utf-8 -*-
 from django.urls import path
+from django.contrib.auth import get_user_model
+from viewflow.contrib.auth import AuthViewset
+from viewflow.urls import Application, Site, ModelViewset
+
+from members import models
+
+site = Site(title="NIMR SACCOS", viewsets=[
+    Application(
+        title="Members", icon="people", app_name="members", viewsets=[
+            ModelViewset(model=models.MemberProfile),
+        ]
+    ),
+])
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('accounts/', AuthViewset(with_profile_view=True).urls),
+    path('', site.urls),
 ]
